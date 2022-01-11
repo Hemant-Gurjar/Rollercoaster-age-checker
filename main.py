bd0158c9de3cd013725233d9d13ba94724138295
Hemant <73576412+Hemant-Gurjar@users.noreply.github.com>
2022-01-11 05:19:30 +0000

    Asking age and telling how much to pay

--- main.py
+++ main.py
@@ -3,5 +3,10 @@ height = int(input("What is your height in cm? "))
 
 if height >= 120:
   print("You can ride the rollercoaster")
+  age = int(input("What is your age? \n"))
+  if age <= 18:
+    print("Please pay $7.")
+  else:
+    print("Please pay $12.")  
 else:
   print("Sorry, you have to grow taller to ride.")  
