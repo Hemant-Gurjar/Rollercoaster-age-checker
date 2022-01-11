eb905a09cd87a8f4c15518c849afa09837206e22
Hemant <73576412+Hemant-Gurjar@users.noreply.github.com>
2022-01-11 08:41:12 +0000

    added bill calculation

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
-
+bill = 0 
 if height >= 120:
   print("You can ride the rollercoaster")
-  age = int(input("What is your age? \n"))
+  age = int(input("What is your age? "))
   if age < 12:
-    print("Please pay $5.")
+    bill = 5
+    print("Your ticket price will be $5.")
   elif age <= 18:
-    print("Please pay $7.")  
+    bill = 7
+    print("Your ticket price will be $7.")  
   else:
-    print("Please pay $12.")  
+    bill = 12
+    print("Your ticket price will be $12.") 
+  want_photo = input("Do you want to have photos taken? Y or N ")
+
+  if want_photo == "Y":
+    bill += 3
+    print(f"Your final bill is ${bill}")
 else:
   print("Sorry, you have to grow taller to ride.")  
