0d281579054bf3fa8110188d30ac387c5fcaf41e
Hemant <73576412+Hemant-Gurjar@users.noreply.github.com>
2022-01-11 05:24:32 +0000

    added new age condition

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ height = int(input("What is your height in cm? "))
 if height >= 120:
   print("You can ride the rollercoaster")
   age = int(input("What is your age? \n"))
-  if age <= 18:
-    print("Please pay $7.")
+  if age < 12:
+    print("Please pay $5.")
+  elif age <= 18:
+    print("Please pay $7.")  
   else:
     print("Please pay $12.")  
 else:
